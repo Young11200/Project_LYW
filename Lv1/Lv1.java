bd2ab05b939994cd2ef4cd34a61b459920d22ea2
이용우 <dcow20@gmail.com>
2024-11-20 08:12:45 +0900

    Exit -> 종료 키워드 변경

--- Lv1/Lv1.java
+++ Lv1/Lv1.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Lv1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); // 사용자 입력을 처리하기 위한 Scanner 객체 생성.
-        String choice; // 사용자의 반복 여부 선택 ("Exit" 또는 계속 입력)을 저장할 변수.
+        String choice; // 사용자의 반복 여부 선택 ("Exit" 또는 계속 입력)을 저장할 변수. - > Exit -> 종료
 
         do { // do-while 반복문: 최소 1회 실행 후 사용자의 선택에 따라 반복 여부를 결정.
             System.out.print("첫 번째 숫자를 입력하세요: "); // 첫 번째 숫자 입력 받기
@@ -46,10 +46,10 @@ public class Lv1 {
             }
 
             // 계산 반복 여부 묻기
-            System.out.print("계속 계산하시겠습니까? (계속하려면 아무 키나 입력하고, 종료하려면 'Exit'를 입력하세요): ");
+            System.out.print("계속 계산하시겠습니까? (계속하려면 아무 키나 입력하고, 종료하려면 '종료'를 입력하세요): ");
             choice = scanner.next(); // 사용자 입력 받기
 
-        } while (!choice.equals("Exit")); // 사용자가 'Exit'를 입력하지 않은 경우 반복. -> 앞에 대문자만 가능....
+        } while (!choice.equals("종료")); // 사용자가 'Exit'를 입력하지 않은 경우 반복. -> 앞에 대문자만 가능.... -> 종료 로 변경
 
         // 프로그램 종료 메시지
         System.out.println("계산기를 종료합니다.");
